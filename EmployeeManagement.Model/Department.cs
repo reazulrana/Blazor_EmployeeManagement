fb820d9b408755e47978126509fd118298dacf50
Reazul Islam <rana12312344@gmail.com>
2024-08-18 11:37:45 +0600

    add initial

---
+++ EmployeeManagement.Model/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Model
+{
+   public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+
+    }
+}
