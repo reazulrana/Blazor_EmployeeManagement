3b68907992d6f3db576f3b1faf9ba1df58b68580
Reazul Islam <rana12312344@gmail.com>
2024-08-27 14:22:36 +0600

    add filter

--- EmployeeManagement.Web/Services/EmployeeService.cs
+++ EmployeeManagement.Web/Services/EmployeeService.cs
@@ -17,13 +17,9 @@ namespace EmployeeManagement.Web.Services
 
         public async Task<Employee> GetEmployee(int id)
         {
-
             string url = $"api/employee/{id}";
-
             var e = await httpClient.GetFromJsonAsync<Employee>(url);
-            
             return e;
-
         }
 
         public async Task<List<Employee>> GetEmployees()
